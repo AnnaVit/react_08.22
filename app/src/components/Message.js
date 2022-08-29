fb56851e6b3ff2df4bbf5980520a70ac3939d3aa
agermashova <agermashova@team.amocrm.com>
2022-08-29 22:12:45 +0300

    min styles

--- app/src/components/Message.js
+++ app/src/components/Message.js
@@ -2,8 +2,8 @@ import './styles/message.css';
 
 export const Message = ({message, author}) => {
 
-    return <>
+    return <div className="message">
         <p>{author}</p>
         <p>{message}</p>
-    </>
+    </div>
 }
